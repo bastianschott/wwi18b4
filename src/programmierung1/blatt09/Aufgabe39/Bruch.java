c7407bfe93d1bc29c032d0aa6d88e8d3b939ecf8
Bastian Schott <b.sch94@gmail.com>
2018-11-15 09:26:00 +0100

    Bruch - Kommentare hinzugefügt

--- src/programmierung1/blatt09/Aufgabe39/Bruch.java
+++ src/programmierung1/blatt09/Aufgabe39/Bruch.java
@@ -1,13 +1,16 @@
 package programmierung1.blatt09.Aufgabe39;
 
 public class Bruch {
+	// Instanzvariablen
 	private int zaehler, nenner;
 	
+	// Methode "ggt()"
+	// Diese Methode gibt einen int-Wert zurück. Daher ist in der Methode auch ein "return" notwendig
 	public int ggt() {
 		if(zaehler == 0) return nenner;
 		if(nenner == 0) return zaehler;
 		int x = Math.abs(zaehler);
-		int y = Math.abs(nenner);
+		int y = Math.abs(nenner); // x und y sind lokale Variablen, welche nur innerhalb der Methode deklariert sind
 		while (x != y) {
 			if(x > y) x -= y;
 			else y -= x;
@@ -15,25 +18,34 @@ public class Bruch {
 		return x;
 	}
 	
-	public Bruch(int zaehler, int nenner) {
+	// Konstruktor mit 2 Parametern
+	// In diesem Konstruktor werden 2 formale Variablen eingegeben und direkt den beiden Instanzvariablen zugewiesen
+	public Bruch(int zaehler, int nenner) { // int ... = formale Variable, welche beim Aufruf des Konstruktors initialisiert werden müssen
 		this.zaehler = zaehler;
 		this.nenner = nenner;
+		// this. = Instanzvariablen, die am Anfang der Klasse deklariert werden
 	}
-	
+
+	// Standardkonstruktor
+	// Hier: Standardkonstruktor ist leer. Theoretisch können jedoch auch hier diverse Operationen vorgenommen werden.
 	public Bruch() {
 	}
-	
+
+	// Überladene Methode:
+	// Methode toString() gibt es bereits in der Klasse "Object", von welcher diese Klasse erbt. Daher ist die Methode hier überladen.
 	public String toString() {
 		return zaehler + "/" + nenner;
 	}
 	
+	// Methode "kuerze()"
+	// Hier wird ein neuer Bruch zurückgegeben
 	public Bruch kuerze() {
-		return new Bruch(
-				zaehler/ggt(),
-				nenner / ggt());
-		
+		return new Bruch(zaehler / ggt(), nenner / ggt());
 	}
 	
+	// Methode "einlesen(x, y)"
+	// Hier mmüssen beim Aufruf 2 int-Werte eingegeben werden, Bsp: "nameDesBruchs.einlesen(5, 3);"
+	// Da die Methode mit "void" gekennzeichnet ist, gibt es hier keinen Rückgabewert. Trotzdem werden hier die beiden Instanzvariablen überschrieben
 	public void einlesen(int zaehler, int nenner) {
 		this.zaehler = zaehler;
 		this.nenner = nenner;
@@ -70,7 +82,5 @@ public class Bruch {
 	public Bruch sub(Bruch b) {
 		return this.add(b.neg()).kuerze();
 	}
-	
-	
 	
 }
