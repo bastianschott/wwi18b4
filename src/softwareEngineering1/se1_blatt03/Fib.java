3432fed27c93ef9f6b5995f04ed370db6607984d
Bastian Schott <b.sch94@gmail.com>
2018-11-21 09:15:05 +0100

    se1_blatt03 (Debugging) hinzugefügt

--- src/softwareEngineering1/se1_blatt03/Fib.java
+++ src/softwareEngineering1/se1_blatt03/Fib.java
@@ -3,13 +3,11 @@ package softwareEngineering1.se1_blatt03;
 import Prog1Tools.IOTools;
 
 public class Fib {
-
 	static int fib(int n) {
 		int f = 0;
 		int fMinus1 = 1;
 		int fMinus2 = -1;
-
-		while (n > 1) {
+		while (n >= 1) {
 			n--;
 			fMinus2 = fMinus1;
 			fMinus1 = f;
@@ -20,6 +18,6 @@ public class Fib {
 
 	public static final void main(final String[] args) {
 		int n = IOTools.readInt("Nte Fibonacci-Zahl f_N berechnen, N = ");
-		System.out.println("f_" + 10 + " = " + fib(n));
+		System.out.println("f_" + n + " = " + fib(n));
 	}
 }
